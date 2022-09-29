394e70e77b4ebaed9e0f882081fed45e8372334c
rayhan3030 <rayhanddd@gmail.com>
2022-09-29 08:50:47 +0600

    Component Folder Created and Food component added and modified added assential code

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ import Food from './components/Food/Food';
 function App() {
   return (
     <div className="App">
+      {/* Header headline added from here  */}
       <h1 ><FontAwesomeIcon icon={faHamburger}></FontAwesomeIcon> P. Hero Foodie War Competition </h1>
       <h3>We have six food item here, Choose wisely so that you can finish it <br />
         on time . Food weight and finishing time is visible on each item.  </h3>
+      {/* Header section end here  */}
       <Food></Food>
     </div>
   );
